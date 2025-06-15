b9505947e6bdd8ed101f57a2d7f4654afab3814a
nehagovekar <nehagovekar2198@gmail.com>
2025-06-15 11:12:18 -0500

    last dataframe remember

---
+++ datainterview_pandas3.py
@@ -0,0 +1,36 @@
+'''Question
+Count the total number of poor quality properties in the AirBnb property dataset.
+Return a single number in a DataFrame with column total_properties.
+
+DataFrame Schema
+Input: airbnb_dim_property
+
++---------------------+-------------+
+| Column Name         | Data Type   |
++---------------------+-------------+
+| property_id         | int64       |
+| owner_id            | int64       |
+| property_type       | object      |
+| property_quality    | object      |
+| no_bedrooms         | int64       |
+| no_bathrooms        | float64     |
+| total_sqft          | object      |
+| nightly_price_range | object      |
+| parking_spots       | int64       |
+| location_town       | object      |
+| location_country    | object      |
+| first_listed_on     | object      |
++---------------------+-------------+
+Expected Output:
+
++-------------------+--------+
+| Column Name       | Type   |
++-------------------+--------+
+| total_properties  | int64  |
++-------------------+--------+'''
+import pandas as pd
+
+# Display the DataFrame for inspection
+total_properties=airbnb_dim_property[airbnb_dim_property['property_quality']=='poor'].shape[0]
+result= pd.DataFrame({"total_properties": [total_properties]})
+result
